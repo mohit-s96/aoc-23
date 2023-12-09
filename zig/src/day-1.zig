ef9f668be22ec536d90fd5f19a3f113e6915ecd2
mohit-s96 <mohitsrivastava67@gmail.com>
2023-12-09 15:53:12 +0530

    day 1

--- zig/src/day-1.zig
+++ zig/src/day-1.zig
@@ -1,3 +1,46 @@
-pub fn sumOfCalibtionValues() i32 {
-    return 69;
+const std = @import("std");
+const io = @import("helpers/io.zig");
+const cli = @import("helpers/cli.zig");
+
+fn charToDigit(c: u8) ?u8 {
+    return switch (c) {
+        '0'...'9' => c - '0',
+        else => null,
+    };
+}
+
+fn parseValue(value: []const u8, allocator: std.mem.Allocator) !std.ArrayList(u8) {
+    var arrayList = std.ArrayList(u8).init(allocator);
+    for (value) |v| {
+        const digit = charToDigit(v);
+        if (digit != null) {
+            try arrayList.append(digit.?);
+        }
+    }
+    return arrayList;
+}
+
+pub fn sumOfCalibrationValues() !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const argValue = cli.extractCliArg("-f");
+    if (argValue == null) std.process.exit(1);
+
+    const contents = try io.readFile(argValue.?, allocator);
+    defer allocator.free(contents);
+
+    var iter = std.mem.splitSequence(u8, contents, "\n");
+
+    var count: usize = 0;
+    while (iter.next()) |line| {
+        const a = try parseValue(line, allocator);
+        defer a.deinit();
+        const first = a.items[0];
+        const last = a.items[a.items.len - 1];
+        count = count + (first * 10 + last);
+    }
+    return count;
 }
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,24 +1,7 @@
 const std = @import("std");
 const day1 = @import("day-1.zig");
-const io = @import("helpers/io.zig");
-const cli = @import("helpers/cli.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
-    defer _ = gpa.deinit();
-
-    const allocator = gpa.allocator();
-
-    const argValue = cli.extractCliArg("-f");
-    if (argValue == null) std.process.exit(1);
-
-    const contents = try io.readFile(argValue.?, allocator);
-    defer allocator.free(contents);
-    std.debug.print("{s}", .{contents});
-    // var iter = std.mem.splitSequence(u8, file_buffer, "\n");
-
-    // var count: usize = 0;
-    // while (iter.next()) |line| : (count += 1) {
-    //     std.log.info("{d:>2}: {s}", .{ count, line });
-    // }
+    const day1Answer = try day1.sumOfCalibrationValues();
+    std.debug.print("Day 1: {d}\n", .{day1Answer});
 }
